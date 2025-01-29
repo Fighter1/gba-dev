8f89df6c87e303b198095ae6ce456552766f9d70
Fighter <Fighter1@users.noreply.github.com>
2025-01-29 00:31:50 -0500

    Bouncing

--- unbricked/main.asm
+++ unbricked/main.asm
@@ -21,27 +21,13 @@ WaitVBlank:
     ld de, Tiles
     ld hl, $9000
     ld bc, TilesEnd - Tiles
-CopyTiles:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTiles
+    call Memcopy
 
     ; Copy the tilemap
     ld de, Tilemap ; Source of the copy
     ld hl, $9800 ; Destination of the copy
     ld bc, TilemapEnd - Tilemap ; Size of the copy
-CopyTilemap:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTilemap
+    call Memcopy
 
     ld a, 0
     ld b, 160
@@ -51,6 +37,7 @@ ClearOam:
     dec b
     jp nz, ClearOam
 
+    ; Initialize paddle sprite in OAM
     ld hl, _OAMRAM
     ld a, 128 + 16
     ld [hli], a
@@ -60,18 +47,33 @@ ClearOam:
     ld [hli], a
     ld [hli], a
 
+    ; Initialize ball sprite in OAM
+    ld a, 100 + 16
+    ld [hli], a
+    ld a, 32 + 8
+    ld [hli], a
+    ld a, 1
+    ld [hli], a
+    ld a, 0
+    ld [hli], a
+
+    ; Set ball momentum
+    ld a, 1
+    ld [wBallMomentumX], a
+    ld a, -1
+    ld [wBallMomentumY], a
+
     ; Copy the paddle tile
     ld de, Paddle
     ld hl, $8000
     ld bc, PaddleEnd - Paddle
-CopyPaddle:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyPaddle
+    call Memcopy
+
+    ; Copy the ball tile
+    ld de, Ball
+    ld hl, $8010
+    ld bc, BallEnd - Ball
+    call Memcopy
 
     ; Turn the LCD on
     ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
@@ -86,6 +88,8 @@ CopyPaddle:
     ; Initialize global variables
     ld a, 0
     ld [wFrameCounter], a
+    ld [wCurKeys], a
+    ld [wNewKeys], a
 
 Main:
     ; Wait until it's *not* VBlank
@@ -96,6 +100,134 @@ WaitVBlank2:
     ld a, [rLY]
     cp 144
     jp c, WaitVBlank2
+
+    ; Add ball's momentum to OAM position
+    ld a, [wBallMomentumX]
+    ld b, a
+    ld a, [_OAMRAM + 5]
+    add a, b
+    ld [_OAMRAM + 5], a
+
+    ld a, [wBallMomentumY]
+    ld b, a
+    ld a, [_OAMRAM + 4]
+    add a, b
+    ld [_OAMRAM + 4], a
+
+    ; Check the current keys every frame and move left or right
+    call UpdateKeys
+
+BounceOnTop:
+    ; Offsest OAM position
+    ; (8, 16) in OAM coords is (0, 0) on screen
+    ld a, [_OAMRAM + 4]
+    sub a, 16 + 1
+    ld c, a
+    ld a, [_OAMRAM + 5]
+    sub a, 8
+    ld b, a
+    call GetTileByPixel ; Return tile address in hl
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceOnRight
+    ld a, 1
+    ld [wBallMomentumY], a
+
+BounceOnRight:
+    ld a, [_OAMRAM + 4]
+    sub a, 16
+    ld c, a
+    ld a, [_OAMRAM + 5]
+    sub a, 8 - 1
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceOnLeft
+    ld a, -1
+    ld [wBallMomentumX], a
+
+BounceOnLeft:
+    ld a, [_OAMRAM + 4]
+    sub a, 16
+    ld c, a
+    ld a, [_OAMRAM + 5]
+    sub a, 8 + 1
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceOnBottom
+    ld a, 1
+    ld [wBallMomentumX], a
+
+BounceOnBottom:
+    ld a, [_OAMRAM + 4]
+    sub a, 16 - 1
+    ld c, a
+    ld a, [_OAMRAM + 5]
+    sub a, 8
+    ld b, a
+    call GetTileByPixel
+    ld a, [hl]
+    call IsWallTile
+    jp nz, BounceDone
+    ld a, -1
+    ld [wBallMomentumY], a
+BounceDone:
+
+PaddleBounce:
+    ; Check if ball low enough
+    ld a, [_OAMRAM]
+    ld b, a
+    ld a, [_OAMRAM + 4]
+    add a, 6 ; Ball bounces when its bottom row of pixels touches paddle's top
+    cp a, b
+    jp nz, PaddleBounceDone ; Ball can't bounce if not at same Y pos as paddle
+    ; Check X pos
+    ld a, [_OAMRAM + 5] ; Ball x pos
+    ld b, a
+    ld a, [_OAMRAM + 1] ; Paddle x pos
+    sub a, 8
+    cp a, b
+    jp nc, PaddleBounceDone
+    add a, 8 + 16 ; 8 to undo, 16 as width
+    cp a, b
+    jp c, PaddleBounceDone
+
+    ld a, -1
+    ld [wBallMomentumY], a
+PaddleBounceDone:
+    
+; Check if left button pressed
+CheckLeft:
+    ld a, [wCurKeys]
+    and a, PADF_LEFT
+    jp z, CheckRight
+Left:
+    ; Move paddle 1 pixel to left
+    ld a, [_OAMRAM + 1]
+    dec a
+    ; Don't move if hit edge of playfield
+    cp a, 15
+    jp z, Main
+    ld [_OAMRAM + 1], a
+    jp Main
+
+    ; Check Right
+CheckRight:
+    ld a, [wCurKeys]
+    and a, PADF_RIGHT
+    jp z, Main
+Right:
+    ; Move paddle 1 pixel to right
+    ld a, [_OAMRAM + 1]
+    inc a
+    ; Don't move if hit edge of playfield
+    cp a, 105
+    jp z, Main
+    ld [_OAMRAM + 1], a
+    jp Main
 
     ld a, [wFrameCounter]
     inc a
@@ -113,6 +245,42 @@ WaitVBlank2:
     ld [_OAMRAM + 1], a
     jp Main
 
+UpdateKeys:
+    ; Poll half the controller
+    ld a, P1F_GET_BTN
+    call .onenibble
+    ld b, a ; B7-4 = 1; B3-0 = unpressed buttons
+    
+    ; Poll the other half
+    ld a, P1F_GET_DPAD
+    call .onenibble
+    swap a ; A3-0 = unpressed directions; A7-4 = 1
+    xor a, b ; A = pressed buttons + directions
+    ld b, a ; B = pressed buttons + directions
+    
+    ; And release the controller
+    ld a, P1F_GET_NONE
+    ldh [rP1], a
+    
+    ; Combine with previous wCurKeys to make wNewKeys
+    ld a, [wCurKeys]
+    xor a, b ; A = keys that changed state
+    and a, b ; A = keys that changed to pressed
+    ld [wNewKeys], a
+    ld a, b
+    ld [wCurKeys], a
+    ret
+    
+    .onenibble
+    ldh [rP1], a ; switch the key matrix
+    call .knownret ; burn 10 cycles calling a known ret
+    ldh a, [rP1] ; ignore value while waiting for the key matrix to settle
+    ldh a, [rP1]
+    ldh a, [rP1] ; this read counts
+    or a, $F0 ; A7-4 = 1; A3-0 = unpressed keys
+    .knownret
+    ret      
+
 ; Copy bytes from one area to another.
 ; @param de: Source
 ; @param hl: Destination
@@ -126,6 +294,56 @@ Memcopy:
     ld a, b
     or a, c
     jp nz, Memcopy
+    ret
+
+; Convert a pixel position to a tilemap address
+; hl = $9800 + X + Y * 32
+; @param b: X
+; @param c: Y
+; @return hl: tile address
+GetTileByPixel:
+    ; First, we need to divide by 8 to convert a pixel position to a tile position.
+    ; After this we want to multiply the Y position by 32.
+    ; These operations effectively cancel out so we only need to mask the Y value.
+    ld a, c
+    and a, %11111000
+    ld l, a
+    ld h, 0
+    ; Now we have the position * 8 in hl
+    add hl, hl ; position * 16
+    add hl, hl ; position * 32
+    ; Convert the X position to an offset.
+    ld a, b
+    srl a ; a / 2
+    srl a ; a / 4
+    srl a ; a / 8
+    ; Add the two offsets together.
+    add a, l
+    ld l, a
+    adc a, h
+    sub a, l
+    ld h, a
+    ; Add the offset to the tilemap's base address, and we are done!
+    ld bc, $9800
+    add hl, bc
+    ret
+
+; @param a: tile ID
+; @return z: set if a is a wall.
+IsWallTile:
+    cp a, $00
+    ret z
+    cp a, $01
+    ret z
+    cp a, $02
+    ret z
+    cp a, $04
+    ret z
+    cp a, $05
+    ret z
+    cp a, $06
+    ret z
+    cp a, $07
     ret
 
 SECTION "Tile data", ROM0
@@ -376,5 +594,24 @@ Paddle:
     dw `00000000
 PaddleEnd:
 
-SECTION "Counter", wram0
+Ball:
+    dw `00033000
+    dw `00322300
+    dw `03222230
+    dw `03222230
+    dw `00322300
+    dw `00033000
+    dw `00000000
+    dw `00000000
+BallEnd:
+
+SECTION "Counter", WRAM0
 wFrameCounter: db
+
+SECTION "Input Variables", WRAM0
+wCurKeys: db
+wNewKeys: db
+
+SECTION "Ball Data", WRAM0
+wBallMomentumX: db
+wBallMomentumY: db
